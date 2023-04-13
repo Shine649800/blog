45bd39be638b1516bca39633fa393261fe9e2a05
zu3x <zu3x@fresnou.org>
2023-04-13 11:02:51 -0700

    sixth commit, finished part 2

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,20 @@ var router = express.Router();
 const articleController = require('../controllers/articleController');
 const commentController = require('../controllers/commentController');
 const userController = require('../controllers/userController');
-
+const ensureUserAuthenticated = require('../middleware/ensureUserAuthenticated');
+const userHadRole = require('../middleware/userHasRole');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/article');
 });
-router.get('/article/add', articleController.renderAddForm);
-router.post('/article/add', articleController.addArticle);
+router.get('/article/add', ensureUserAuthenticated, userHadRole, articleController.renderAddForm);
+router.post('/article/add', ensureUserAuthenticated, userHadRole, articleController.addArticle);
 
 router.get('/article/:articleId', articleController.displayArticle);
 router.get('/article',articleController.displayAll);
-router.get('/article/:articleId/edit', articleController.renderEditForm);
-router.post('/article/:articleId/edit', articleController.updateArticle);
-router.get('/article/:articleId/delete', articleController.deleteArticle);
+router.get('/article/:articleId/edit', ensureUserAuthenticated, userHadRole, articleController.renderEditForm);
+router.post('/article/:articleId/edit', ensureUserAuthenticated, userHadRole, articleController.updateArticle);
+router.get('/article/:articleId/delete', ensureUserAuthenticated, articleController.deleteArticle);
 
 router.post('/article/:articleId/comment/create', commentController.createComment);
 router.post('/comment/:commentId/reply/create', commentController.addReply);
@@ -26,4 +27,7 @@ router.post('/register', userController.register);
 router.get('/login', userController.renderLogin);
 router.post('/login', userController.login);
 router.get('/logout', userController.logout);
+
+router.get('/comment/:commentId/delete', ensureUserAuthenticated, userHadRole, commentController.deleteComment);
+router.get('/comment/:commentId/reply/:replyId/delete', ensureUserAuthenticated, userHadRole, commentController.deleteReply);
 module.exports = router;
